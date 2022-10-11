671a7e56ca66d6224409de1c68bc27b7d6fc69f5
SeppeVDHH <seppe.vanderhallen@hotmail.com>
2022-10-11 11:46:23 +0200

    Interface v1

---
+++ src/hotel/IBookingManager.java
@@ -0,0 +1,15 @@
+package hotel;
+
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+import java.time.LocalDate;
+import java.util.Set;
+
+public interface IBookingManager extends Remote {
+    public boolean isRoomAvailable(Integer roomNumber, LocalDate date) throws RemoteException;
+
+    public void addBooking(BookingDetail bookingDetail) throws RemoteException;
+
+    public Set<Integer> getAvailableRooms(LocalDate date) throws RemoteException;
+
+}
